ce833ba6843156fd9a81c10d5b9e05ba5f2710af
Atalayagiz <78467521+Atalayagiz@users.noreply.github.com>
2022-06-03 15:24:05 +0300

    Add files via upload

---
+++ WatchDog/WatchDog/Form1.cs
@@ -0,0 +1,40 @@
+﻿using MonitoredApplication;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WatchDog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //https://www.turkhackteam.org/uye/endarion.955141/
+
+
+            //Application Watcher uygulamayı belirtilen intervalda yeniden başlatır.
+            ApplicationWatcher applicationWatcher = new ApplicationWatcher("notepad", "WatchDog", 5000);
+
+            //Watchdog Watcher uygulama'nın çalışıp çalışmadığını belirtilen interval'da kontrol eder. Eğer ki çalışıyorsa herhangi bir şey yapmaz. Çalışmıyorsada çalıştırır.
+            WatchdogWatcher watchdogWatcher = new WatchdogWatcher("notepad", "notepad.exe", 5000);
+
+
+
+
+
+
+        }
+    }
+}
